73c6d7ad3024432d403bbed9fa8c983866d009a6
wflixu <wflixu@88.com>
2025-11-23 22:45:02 +0800

    Initial Commit

---
+++ LifeCultivationTests/LifeCultivationTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCultivationTests.swift
+//  LifeCultivationTests
+//
+//  Created by luke on 2025/11/23.
+//
+
+import Testing
+@testable import LifeCultivation
+
+struct LifeCultivationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
